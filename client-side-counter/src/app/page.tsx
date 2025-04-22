e7ef04d8ece828e3bc8fbed5e5421d297dc571e3
cristian-rla <Cristianrluquea@hotmail.com>
2025-04-22 01:27:19 -0600

    api now works

--- client-side-counter/src/app/page.tsx
+++ client-side-counter/src/app/page.tsx
@@ -5,9 +5,9 @@ import { useState } from "react";
 export default function Home() {
 
   const handleSubmit = () =>{
-    fetch(`http://localhost:3000/api/operacion?num1=${num1}&num2=${num2}&opp=${operator}`)
+    fetch(`http://localhost:3001/api/operacion?num1=${num1}&num2=${num2}&opp=${operator}`)
     .then(res => res.json())
-    .then(data => setResult(data.opResult));
+    .then(data => setResult(data.result));
     
   };
 
@@ -17,13 +17,13 @@ export default function Home() {
   const [result, setResult] = useState(null);
 
   return (
-    <div className="flex flex-col items-center p-6 bg-gray-100 min-h-screen">
-      <div className="self-center flex gap-2 mb-4">
+    <div className="self-center flex flex-col items-center p-6 bg-gray-100 min-h-screen">
+      <div className="flex gap-2 mb-4">
         <input
           type="number"
           value={num1}
           onChange={(e) => setNum1(e.target.value)}
-          className="p-2 border rounded border-gray-500"
+          className="p-2 border rounded border-gray-500 text-black"
           placeholder="Número 1"
         />
         <select
@@ -31,16 +31,16 @@ export default function Home() {
           onChange={(e) => setOperator(e.target.value)}
           className="p-2 border rounded border-gray-500 text-black"
         >
-          <option className ="" value="+">+</option>
-          <option value="-">-</option>
-          <option value="*">×</option>
-          <option value="/">÷</option>
+          <option value="sum">+</option>
+          <option value="sub">-</option>
+          <option value="mul">×</option>
+          <option value="div">÷</option>
         </select>
         <input
           type="number"
           value={num2}
           onChange={(e) => setNum2(e.target.value)}
-          className="p-2 border rounded border-gray-500"
+          className="p-2 border rounded border-gray-500 text-black"
           placeholder="Número 2"
         />
       </div>
@@ -50,8 +50,8 @@ export default function Home() {
       >
         Calcular
       </button>
-      <div className="mt-4 text-lg font-semibold">
-        {result !== null && <p>Resultado: {result}</p>}
+      <div className="mt-4 text-lg font-semibold text-black">
+        {result && <span>Resultado: {result}</span>}
       </div>
     </div>
   );
--- counter-api/index.js
+++ counter-api/index.js
@@ -1,25 +1,32 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
-app.get("/api/operacion", (req, res) => {
+app.use(cors())
+app.get("/api/operacion", async (req, res) => {
     const {num1, num2, opp} = req.query;
+    const a = Number(num1);
+    const b = Number(num2);
+    console.log(a,b,opp)
     let opResult;
     switch (opp){
-        case "+":
-            opResult = num1 + num2;
+        case "sum":
+            opResult = a + b;
             break;
-        case "-":
-            opResult = num1 - num2;
+        case "sub":
+            opResult = a - b;
             break;
-        case "/":
-            opResult = num1 / num2;
+        case "div":
+            opResult = a / b;
             break;
-        case "*":
-            opResult = num1 * num2;
+        case "mul":
+            opResult = a * b;
             break;
     }
-    res.json({result: opResult});
+    console.log(opResult);
+    res.status(200).json({result: opResult});
 
 });
 
-app.listen(3000)
+PORT = process.env.PORT || 3001
+app.listen(PORT, () => console.log("Escuchando en el puerto " + PORT))
